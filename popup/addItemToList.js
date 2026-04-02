d48e2861e7f291882a0e22dc41c367cfc88083e2
Samuel Stanley <148486731+samuelstanley0101@users.noreply.github.com>
2026-04-02 00:43:22 -0400

    added current URL logging on click

--- popup/addItemToList.js
+++ popup/addItemToList.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const btn = document.getElementById('addItemToList');
     if (!btn) return;
 
-    btn.addEventListener('click', function () {
-        console.log("Added item to list!")
+    btn.addEventListener('click', async function () {
+        // get the current url
+        const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+        console.log("Adding item to list from URL:", tab.url);
 
         // after the script runs, disable the button and change its text
         this.disabled = true;
