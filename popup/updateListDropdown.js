bc5c99a9535ffb3e9aa0cb1f6c2bcf20151a0662
Samuel Stanley <148486731+samuelstanley0101@users.noreply.github.com>
2026-04-15 23:26:21 -0400

    partially implemented updateListDropdown.js

--- popup/updateListDropdown.js
+++ popup/updateListDropdown.js
@@ -1,1 +1,34 @@
+async function getWishLists() {
+    // stubbed out getLists function
+    const wishLists = [];
+
+    // push fetched wish lists to the list
+    wishLists.push('birthday');
+    wishLists.push('mom');
+    wishLists.push('christmas');
+
+    return wishLists;
+}
+
+async function updateDropdown() {
+    // get the dropdown menu from the HTML DOM
+    const dropdown = document.getElementById('list-select');
+
+    // get the wish lists from the server
+    wishLists = await getWishLists();
+
+    // replace options with wish lists fetched from the server
+    while (dropdown.options.length > 0) {
+        dropdown.remove(0);
+    }
+    for (var i = 0; i < wishLists.length; i++) {
+        let newOption = document.createElement('option');
+        newOption.value = wishLists[i];
+        newOption.text = wishLists[i];
+        dropdown.appendChild(newOption);
+    }
+}
+
 console.log("Attempting to update the dropdown list from the server.");
+
+updateDropdown();
